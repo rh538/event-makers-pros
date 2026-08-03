bd06d453a5508bd0a4d069b652941c1d27a4e3c3
rh538 <rh@eventualidades.pt>
2026-08-03 20:10:49 +0100

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-pimport { createFileRoute, Link } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 import { useState } from "react";
 import {
   ArrowRight,
